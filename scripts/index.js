36634704d352ab4e496ef20fd2502804fa543f57
Alexander Yudin <Yudin.A.O@yandex.ru>
2024-02-24 14:21:49 +0400

    Mesto

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,6 @@
 const cardTemplate = document.querySelector("#card-template").content;
 const placesList = document.querySelector(".places__list");
 const addButton = document.querySelector(".profile__add-button");
-let currentCardIndex = 0;
 
 function createCard(cardData) {
   const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
@@ -10,23 +9,29 @@ function createCard(cardData) {
   placesList.append(cardElement);
 }
 
-addButton.addEventListener("click", function () {
-  if (currentCardIndex < initialCards.length) {
-    createCard(initialCards[currentCardIndex]);
-    currentCardIndex++;
-  }
+initialCards.forEach(function(cardData) {
+  createCard(cardData);
 });
+
+placesList.addEventListener("click", (event) => {
+if (event.target.classList.contains("card__delete-button")) {
+  const card = event.target.closest(".places__item");
+  card.remove();
+}
+});
+
+// если нужно добавлять карточки по 1
+// let currentCardIndex = 0;
+// addButton.addEventListener("click", function () {
+//   if (currentCardIndex < initialCards.length) {
+//     createCard(initialCards[currentCardIndex]);
+//     currentCardIndex++;
+//   }
+// });
 //  если нужно добавлять все карточки сразу, этот код раскомментировать. Сверху код удалить
 // addButton.addEventListener("click", function () {
 //   initialCards.forEach(function (cardData) {
 //     createCard(cardData);
 //   });
 // });
-
-placesList.addEventListener("click", (event) => {
-  if (event.target.classList.contains("card__delete-button")) {
-    const card = event.target.closest(".places__item");
-    card.remove();
-  }
-});
 
