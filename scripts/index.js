6f439fb48ba98008915422822f0679f6ba3a40f3
Alexander Yudin <Yudin.A.O@yandex.ru>
2024-02-24 13:51:47 +0400

    Mesto

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,32 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
+const placesList = document.querySelector(".places__list");
+const addButton = document.querySelector(".profile__add-button");
+let currentCardIndex = 0;
 
-// @todo: DOM узлы
+function createCard(cardData) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  cardElement.querySelector(".card__image").src = cardData.link;
+  cardElement.querySelector(".card__title").textContent = cardData.name;
+  placesList.append(cardElement);
+}
 
-// @todo: Функция создания карточки
+addButton.addEventListener("click", function () {
+  if (currentCardIndex < initialCards.length) {
+    createCard(initialCards[currentCardIndex]);
+    currentCardIndex++;
+  }
+});
+//  если нужно добавлять все карточки сразу, этот код раскомментировать. Сверху код удалить
+// addButton.addEventListener("click", function () {
+//   initialCards.forEach(function (cardData) {
+//     createCard(cardData);
+//   });
+// });
 
-// @todo: Функция удаления карточки
+placesList.addEventListener("click", (event) => {
+  if (event.target.classList.contains("card__delete-button")) {
+    const card = event.target.closest(".places__item");
+    card.remove();
+  }
+});
 
-// @todo: Вывести карточки на страницу
